a57cf7e3c26662fa48e8e2fa08b8ad0a507fc236
kaphelps33 <alexphelps2001@gmail.com>
2024-10-28 17:48:06 -0400

    hobby page

--- itis3135/hobby/scripts/navigation.js
+++
@@ -1,6 +0,0 @@
-function showSection(id) {
-  document.querySelectorAll("main").forEach((section) => {
-    section.classList.remove("active");
-  });
-  document.getElementById(id).classList.add("active");
-}
